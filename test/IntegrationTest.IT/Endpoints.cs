6168afdd19cd6289bd6adf8ea95ba32f1c91ce4b
Nacho Fanjul <nfanjul@plainconcepts.com>
2019-06-12 15:30:33 +0200

    test create team

--- test/IntegrationTest.IT/Endpoints.cs
+++ test/IntegrationTest.IT/Endpoints.cs
@@ -4,4 +4,9 @@
     {
         public static string GetAllTeams => $"api/teams";
     }
+
+    public static class Post
+    {
+        public static string Team => $"api/teams";
+    }
 }
--- test/IntegrationTest.IT/Scenarios/TeamScenarios.cs
+++ test/IntegrationTest.IT/Scenarios/TeamScenarios.cs
@@ -1,8 +1,11 @@
+using IntegrationTest.Api.Models.Teams;
 using IntegrationTest.Entities;
 using IntegrationTest.IT.Attributes;
 using Newtonsoft.Json;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -27,5 +30,24 @@ namespace IntegrationTest.IT.Scenarios
             var result = JsonConvert.DeserializeObject<List<Team>>(json);
             Assert.True(result.Any());
         }
+
+        [Fact]
+        [Reset]
+        public async Task Create_New_Team_And_Return_OK()
+        {
+            var team = new CreateTeam()
+            {
+                Name = "UP Langreo",
+            };
+            var content = new StringContent(JsonConvert.SerializeObject(team), UTF8Encoding.UTF8, "application/json");
+            var response = await _fixture.Server.CreateClient()
+                .PostAsync(Post.Team, content);
+
+            response.EnsureSuccessStatusCode();
+            var json = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<Team>(json);
+            Assert.Equal("UP Langreo", result.Name);
+        }
+
     }
 }
